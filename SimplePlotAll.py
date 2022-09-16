39c23cc6deda07a27ae21758d9cc97301a4093da
mldion <matthew.l.dion@gmail.com>
2022-09-16 12:39:30 -0700

    plots current range QA data sequentially, but grouped by resistor; then plots all gain stage QA data all at once

--- SimplePlotAll.py
+++ SimplePlotAll.py
@@ -3,6 +3,7 @@ from sys import argv
 import pandas as pd
 import os
 from typing import Tuple
+from typing import List
 
 LOGLOG = True
 SEMILOG = False
@@ -29,7 +30,7 @@ def findBodeNames(data: pd.DataFrame) -> Tuple[str, str, str]:
             break
     return (freqHdrStr, ZmodHdrStr, PhaseHdrStr)
 
-def plotData(filename: str) -> None:
+def plotOne(filename: str) -> None:
         data: pd.DataFrame = pd.read_csv(filename)
         frequency_str, impedance_str, phase_str = findBodeNames(data)
 
@@ -58,38 +59,72 @@ def plotData_append(filename: str, canvas) -> None:
         data: pd.DataFrame = pd.read_csv(filename)
         frequency_str, impedance_str, phase_str = findBodeNames(data)
 
-        #Average the results
-        data = data.groupby(frequency_str).mean().reset_index()
-
         shortFilename = filename.split('\\')[-1]
 
         canvas[0].plot(data[frequency_str], data[impedance_str], label = impedance_str,linestyle='--',marker='o')
         canvas[1].plot(data[frequency_str], data[phase_str], label = phase_str,linestyle='--',marker='o')
 
-def main():
-    rootDir = argv[1]
+def plot_sequentially(rootDir: str, filterStr:str = None):
     for root, dirs, files in os.walk(rootDir):
         for name in files:
-            plotData(os.path.join(root, name))
+            if filterStr is None:
+                plotOne(os.path.join(root, name))
+            elif filterStr in name:
+                plotOne(os.path.join(root, name))
 
-def main2():
-    rootDir = argv[1]
-    fig, canvas = plt.subplots(2)
-    if LOGLOG:
+def plot_all(rootDir: str, filterStr:str = None, plotTitle: str = None):
+    plotList: List[str] = []
+    for root, dirs, files in os.walk(rootDir):
+        for name in files:
+            if filterStr is None:
+                plotList.append(os.path.join(root, name))
+            elif filterStr in name:
+                plotList.append(os.path.join(root, name))
+    
+    if len(plotList) > 0:
+        fig, canvas = plt.subplots(2)
+        if LOGLOG:
             canvas[0].set_xscale("log")
             canvas[0].set_yscale("log")
             canvas[1].set_xscale("log")
-    elif SEMILOG:
-        canvas[0].set_xscale("log")
-        canvas[1].set_xscale("log")
-    for root, dirs, files in os.walk(rootDir):
-        for name in files:
-            plotData_append(os.path.join(root, name), canvas)
-    canvas[0].legend()
-    canvas[1].legend()
-    plt.show(block=True)
+        elif SEMILOG:
+            canvas[0].set_xscale("log")
+            canvas[1].set_xscale("log")
+        for item in plotList:
+            plotData_append(item, canvas)
+        if plotTitle is not None:
+            canvas[0].set_title(plotTitle)
+        canvas[0].legend()
+        canvas[1].legend()
+        plt.show(block=True)
 
 
 if __name__ == '__main__':
-    #main()
-    main2()
+    dumbPlot = False
+    sequencedPlot = True
+
+    for dirStr in argv[1:]:
+
+        if dumbPlot:
+            plot_sequentially(dirStr)
+            #plot_all(dirStr)
+
+        if sequencedPlot:
+            #plot current ranges
+            impedanceGroups: Tuple[str] = (
+                "R100milliOhm", "R1Ohm", "R10Ohm", "R100Ohm",
+                "R1kOhm", "R10kOhm", "R100kOhm",
+                "R1MOhm", "R10MOhm", "R100MOhm",
+                "R1GOhm", "R10GOhm", "R50GOhm"
+            )
+
+            currentRangeQADir = dirStr + '/ch1/QCTests_AC/CurrentRanges'
+            gainStageQADir = dirStr + '/ch1/QCTests_AC/GainStages'
+            
+            for impedanceStr in impedanceGroups:
+                plot_all(currentRangeQADir, filterStr=impedanceStr, plotTitle=impedanceStr)
+            
+            #plot gain stages
+            plot_all(gainStageQADir, plotTitle="Gain Stages")
+
+        print(f'Files in {dirStr} done plotting.')
